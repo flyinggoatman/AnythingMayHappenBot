68e1ade7ec9474f5c5570c915bfeb8d0d8d60712
flyinggoatman <flyinggoatman@outlook.com>
2023-01-22 20:42:00 +0000

    added a delete command and a setchannel command.

--- AnythingMayHappenBot.py
+++ AnythingMayHappenBot.py
@@ -14,21 +14,12 @@ bot = commands.Bot(command_prefix='! ', intents=intents)
 SQL_HOST, SQL_USER, SQL_PASS, SQL_DATABASE, DISCORD_CHANNEL_ID, TOKEN = env_pull()
 
 # create an engine to connect to the database
-engine = create_engine(f'mysql://{SQL_USER}:{SQL_PASS}@{SQL_HOST}/{SQL_DATABASE}')
 
-
-Base = declarative_base()
-class Link(Base):
-    __tablename__ = 'link'
-    id = Column(Integer, primary_key=True)
-    link = Column(String)
-Base.metadata.create_all(engine)
 
 
 
 # create a session to execute queries
-Session = sessionmaker(bind=engine)
-session = Session()
+allowed_channels = []
 
 @bot.event
 async def on_ready():
@@ -38,19 +29,56 @@ async def on_message(message):
     guild_id = message.guild.id
     author = message.author
     mention = author.mention
+    discord_name = author.name
     if message.author == bot.user:
         return
     
+    
     DISCORD_CHANNEL_ID_int = int(DISCORD_CHANNEL_ID)
-    if DISCORD_CHANNEL_ID_int == message.channel.id:
-        link = message.content
+    if message.content.startswith("!setchannel"):
+        channel_id = int(message.content.split()[1])
+        allowed_channels.append(channel_id)
+        await message.channel.send(f"Channel with ID {channel_id} has been added to the list of allowed channels.")
+    elif message.channel.id in DISCORD_CHANNEL_ID_int:
         
+        
+        engine = create_engine(f'mysql://{SQL_USER}:{SQL_PASS}@{SQL_HOST}/{SQL_DATABASE}')
+
+
+        Base = declarative_base()
+        class Link(Base):
+            __tablename__ = 'link'
+            id = Column(Integer, primary_key=True)
+            link = Column(String)
+        Base.metadata.create_all(engine)
+        
+        
+        
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        
+        link = message.content
+        if message.content.startswith('!delete'):
+            await message.delete()
+            link = message.content.split()[1]
+            link = link.replace("http://", "").replace("https://", "")
+            result = session.query(Link).filter_by(link=link).first()
+            if not result:
+                await message.channel.send(f"{link} is not present in the database {mention}.", delete_after=10)
+                print(f"{link} is not present in the database {discord_name}.")
+            else:
+                session.delete(result)
+                session.commit()
+                await message.channel.send(f"{link} has been removed from the database {mention}.", delete_after=10)
+                print(f"{link} has been removed from the database {discord_name}.")
+
         
         # check if the link is a valid URL
         match = re.match(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', link)
+        match_2 = re.match(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]+\.[a-zA-Z0-9()]{2,6}[/a-zA-Z0-9._?=\-&]+', link)
         url = link
-        link = link.replace("https://", "")
-        if match:
+        link = link.replace("http://", "").replace("https://", "")
+        if match or match_2:
             result = session.query(Link).filter_by(link=link).first()
             if not result:
                 
@@ -58,11 +86,12 @@ async def on_message(message):
                 session.add(new_link)
                 session.commit()
                 await message.delete()
-                await message.channel.send(f"Link {message.content} added successfully!")
+                await message.channel.send(f"{message.content} added successfully!")
                 print(f"Link {message.content} added successfully!")
             else: 
                 await message.channel.send(f"{message.content} is already in the database {mention}.", delete_after=10)
                 await message.delete()
                 print(f"{message.content} is already in the database.")
+
 
 bot.run(TOKEN)
